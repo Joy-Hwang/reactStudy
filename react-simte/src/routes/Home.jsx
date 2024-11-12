ae0a3b8600c438eb3812f2e2b4400ecee748a652
Joy-Hwang <122965922+Joy-Hwang@users.noreply.github.com>
2024-11-12 16:06:13 +0900

    wip

--- react-simte/src/routes/Home.jsx
+++ react-simte/src/routes/Home.jsx
@@ -1,13 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Button from "../components/Button"; // 추가
 const Home = () => {
+  let navigate = useNavigate();
+  const startTest = () => {
+    navigate(`/subpage`);
+  };
   return (
     <>
       {" "}
       <main className="">
         {" "}
         <h1>React Simte</h1> <Link to={`/subpage`}>Go to subpage &rarr;</Link>{" "}
-        <Button btntext={"시작"} onclick />
+        <Button btntext={"시작"} onClickEvent={startTest} />{" "}
       </main>{" "}
     </>
   );
